223760549d699de8b41a65d952ce0f5f53919c83
shubhampachghare <shubhampachghare@gmail.com>
2023-07-01 14:44:30 +0530

    [Shubham] -InheritanceExamaple

---
+++ Inheritance/Vehicle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Vehicle
+    {
+        public string brand = "Ford";
+
+        public void Honk()
+        {
+            Console.WriteLine("Tuee Tueee");
+        }
+    }
+
+    class Car : Vehicle 
+    {
+        public string modelName = "Mustang";
+
+    }
+}
